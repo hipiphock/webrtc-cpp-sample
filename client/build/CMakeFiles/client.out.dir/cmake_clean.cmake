058ffd92f4e872d35cc67b06b2f5c4ef1f4fb10b
hingook_at_lab <hipiphock@naver.com>
2020-02-12 22:40:48 +0900

    merge complete

---
+++ client/build/CMakeFiles/client.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.out.dir/client.cpp.o"
+  "client.out.pdb"
+  "client.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
